c2544a6c77a465c95630bbf1af54ae1c8ed7dfad
penicili <bagaskoro55@gmail.com>
2025-07-07 13:21:19 +0700

    tambahin x-icon ke detail dan ubah judul

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail</title>
+    <title>Detail - {{ $workspace->name }}</title>
+    <link rel="icon" href="{{ asset('images/squarelogo.png') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="{{asset('js/detail.js')}}"></script>
 
